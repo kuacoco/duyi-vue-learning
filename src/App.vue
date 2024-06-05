309a31a4056ef9773d02c14e556fb82775671483
kuacoco <kuacoco@qq.com>
2024-06-05 16:09:14 +0800

    Icon

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <script>
 import AvatarImg from '@/components/AvatarImg.vue'
+import IconImg from '@/components/IconImg.vue'
 
 export default {
   name: 'App',
   components: {
-    AvatarImg
+    AvatarImg,
+    IconImg
   }
 }
 </script>
 
 <template>
   <AvatarImg url="https://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg" :size="100" />
+  <IconImg type="home" />
 </template>
 
-<style scoped></style>
+<style scoped>
+.iconfont {
+  color: aquamarine;
+  font-size: 26px;
+}
+</style>
