9679b954b71ce9a569602e0c7b65e96018002e0a
kuacoco <kuacoco@qq.com>
2024-07-04 14:41:47 +0800

    Empty组件

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,15 @@
 import Avatar from '@/components/Avatar.vue'
 import Icon from '@/components/Icon.vue'
 import Pager from '@/components/Pager.vue'
+import Empty from '@/components/Empty.vue'
 
 export default {
   name: 'App',
   components: {
     Avatar,
     Icon,
-    Pager
+    Pager,
+    Empty
   },
   data: function () {
     return {
@@ -27,6 +29,9 @@ export default {
   <Avatar url="https://qiheizhiya.oss-cn-shenzhen.aliyuncs.com/image/avatar8.jpg" :size="100" />
   <Icon type="qq" />
   <Pager :current="current" :total="128" :limit="10" :visibleNumber="5" @page-change="pageChange" />
+  <div style="background-color: antiquewhite; border: 1px solid #dc6a12; height: 500px; width: 500px">
+    <Empty />
+  </div>
 </template>
 
 <style scoped>
---
+++ src/components/Empty.vue
@@ -0,0 +1,34 @@
+<script>
+import Icon from '@/components/Icon.vue'
+
+export default {
+  name: 'Empty',
+  components: { Icon },
+  props: {
+    text: {
+      type: String,
+      default: '无数据'
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="empty-container">
+    <Icon type="empty" />
+    <div>{{ text }}</div>
+  </div>
+</template>
+
+<style scoped>
+.empty-container {
+  width: 100px;
+  text-align: center;
+  margin: 0 auto;
+}
+
+.icon-container {
+  font-size: 100px;
+  color: var(--vt-c-text-light-2);
+}
+</style>
