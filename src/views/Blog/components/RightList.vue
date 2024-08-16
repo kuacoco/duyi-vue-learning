508be335fda8a6f13a5fe82bc7147ca0cb4743df
kuacoco <kuacoco@qq.com>
2024-08-16 10:38:42 +0800

    组件迭代

---
+++ src/views/Blog/components/RightList.vue
@@ -0,0 +1,43 @@
+<script setup>
+const props = defineProps({
+  list: Array
+})
+const emit = defineEmits(['select'])
+
+const clickHandler = (e) => {
+  emit('select', e)
+}
+</script>
+
+<template>
+  <ul class="right-list-container">
+    <li v-for="(li, i) in list" :key="i">
+      <span @click="clickHandler(li)" :class="{ active: li.isSelect }">{{ li.name }}</span>
+      <RightList :list="li.children" @select="clickHandler" />
+    </li>
+  </ul>
+</template>
+
+<style scoped lang="less">
+@import '~@/styles/var';
+
+.right-list-container {
+  list-style: none;
+  padding: 0;
+
+  .right-list-container {
+    margin-left: 1em;
+  }
+
+  li {
+    min-height: 40px;
+    line-height: 40px;
+    cursor: pointer;
+
+    .active {
+      color: @warn;
+      font-weight: bold;
+    }
+  }
+}
+</style>
--- src/views/Blog/index.vue
+++ src/views/Blog/index.vue
@@ -1,9 +1,62 @@
 <template>
-  <h1>文章</h1>
+  <div class="blog-container">
+    <h1>文章</h1>
+    <div class="list-container">
+      <RightList :list @select="selectHandler" />
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+import RightList from '@/views/Blog/components/RightList.vue'
+
+export default {
+  components: {
+    RightList
+  },
+  data() {
+    return {
+      list: [
+        { name: 'a', isSelect: false },
+        { name: 'b', isSelect: false },
+        {
+          name: 'c',
+          isSelect: true,
+          children: [
+            { name: 'c-1', isSelect: false },
+            {
+              name: 'c-2',
+              isSelect: false,
+              children: [
+                { name: 'c-2-1', isSelect: false },
+                { name: 'c-2-2', isSelect: false },
+                { name: 'c-2-3', isSelect: false },
+                { name: 'c-2-4', isSelect: false }
+              ]
+            },
+            { name: 'c-3', isSelect: false },
+            { name: 'c-4', isSelect: false }
+          ]
+        },
+        { name: 'd', isSelect: false }
+      ]
+    }
+  },
+  methods: {
+    selectHandler: function (item) {
+      console.log(item)
+    }
+  }
+}
 </script>
 
-<style></style>
+<style scoped lang="less">
+.blog-container {
+  height: 100%;
+  display: flex;
+
+  .list-container {
+    flex: 1;
+  }
+}
+</style>
