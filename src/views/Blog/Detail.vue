1d35d104f038d339dbf9a231fa05c936c50893ab
kuacoco <kuacoco@qq.com>
2024-08-19 16:16:11 +0800

    文章详情页main部分

--- src/views/Blog/Detail.vue
+++ src/views/Blog/Detail.vue
@@ -1,7 +1,49 @@
-<script setup></script>
+<script setup>
+import BlogDetail from '@/views/Blog/components/BlogDetail.vue'
+import BlogTOC from '@/views/Blog/components/BlogTOC.vue'
+import Layout from '@/components/Layout.vue'
+import { getBlog } from '@/api/blog.js'
+import { useFetch } from '@/composables/fetch.js'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const { isLoading, data } = useFetch(fetchData)
+
+function fetchData() {
+  return getBlog(route.params.id)
+}
+</script>
 
 <template>
-  <h1>详情</h1>
+  <Layout>
+    <div class="main-container" v-loading="isLoading">
+      <BlogDetail :data v-if="data" />
+    </div>
+    <template #right>
+      <div class="right-container" v-loading="isLoading">
+        <BlogTOC :toc="data.toc" v-if="data" />
+      </div>
+    </template>
+  </Layout>
 </template>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.main-container {
+  overflow-y: scroll;
+  height: 100%;
+  box-sizing: border-box;
+  padding: 20px;
+  position: relative;
+  width: 100%;
+  overflow-x: hidden;
+  scroll-behavior: smooth;
+}
+.right-container {
+  width: 300px;
+  height: 100%;
+  overflow-y: scroll;
+  box-sizing: border-box;
+  position: relative;
+  padding: 20px;
+}
+</style>
---
+++ src/views/Blog/components/BlogDetail.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { formatDateTime } from '@/utils'
+import 'highlight.js/styles/github.css'
+import '@/styles/markdown.css'
+
+const props = defineProps({
+  data: { type: Object, required: true }
+})
+</script>
+
+<template>
+  <div class="blog-detail-container">
+    <h1>{{ data.title }}</h1>
+    <div class="aside">
+      <span>日期: {{ formatDateTime(data.createDate) }}</span>
+      <span>浏览: {{ data.scanNumber }}</span>
+      <a href="">评论: {{ data.commentNumber }}</a>
+      <a href="">{{ data.category.name }}</a>
+    </div>
+    <div v-html="data.htmlContent" class="markdown-body"></div>
+  </div>
+</template>
+
+<style scoped lang="less">
+@import '~@/styles/var.less';
+
+.aside {
+  font-size: 12px;
+  color: @gray;
+
+  span,
+  a {
+    margin-right: 15px;
+  }
+}
+
+.markdown-body {
+  margin: 2em 0;
+}
+</style>
