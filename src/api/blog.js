1d61aa1fcaca6c266d4aaca2b12ae10faf8faf1c
kuacoco <kuacoco@qq.com>
2024-08-19 14:57:46 +0800

    mock数据

--- src/api/blog.js
+++ src/api/blog.js
@@ -13,3 +13,21 @@ export async function getBlogList(page = 1, limit = 10, categoryId = -1) {
 export async function getBlogCategories() {
   return await request.get('/api/blogCategories')
 }
+
+export async function getBlog(id) {
+  return await request.get('/api/blog/' + id)
+}
+
+export async function postComment(commentInfo) {
+  return await request.post('/api/comment', commentInfo)
+}
+
+export async function getCommentList(page = 1, limit = 10, blogId = -1) {
+  return await request.get('/api/comment', {
+    params: {
+      page,
+      limit,
+      blogId
+    }
+  })
+}
