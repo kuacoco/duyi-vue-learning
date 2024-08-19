865d231dab5bf6c631c84f19601ecbebb23f8786
kuacoco <kuacoco@qq.com>
2024-08-19 16:42:57 +0800

    文章详情页toc部分

--- src/views/Blog/components/BlogTOC.vue
+++ src/views/Blog/components/BlogTOC.vue
@@ -1,7 +1,36 @@
-<script setup></script>
+<script setup>
+import RightList from '@/views/Blog/components/RightList.vue'
+import { computed } from 'vue'
+import { useRoute } from 'vue-router'
+import { useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+const props = defineProps({
+  toc: {
+    type: Array,
+    required: true
+  }
+})
+
+const list = computed(() => {
+  const mapFn = (item) => {
+    return {
+      isSelect: route.hash.substring(1) === item.anchor,
+      ...item,
+      children: item.children ? item.children.map(mapFn) : undefined
+    }
+  }
+  return props.toc.map(mapFn)
+})
+
+function selectHandler(e) {
+  location.hash = e.anchor
+}
+</script>
 
 <template>
-  <h1>toc</h1>
+  <RightList :list @select="selectHandler" />
 </template>
 
 <style scoped lang="less"></style>
