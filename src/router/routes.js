ece6aad60d6596cfdb5b2a3b2eb114b8795e64f6
kuacoco <kuacoco@qq.com>
2024-08-26 17:45:14 +0800

    动态路由

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,70 @@
-import Home from '@/views/Home/index.vue'
-import About from '@/views/About/index.vue'
-import Blog from '@/views/Blog/index.vue'
-import BlogDetail from '@/views/Blog/Detail.vue'
-import Message from '@/views/Message/index.vue'
-import Project from '@/views/Project/index.vue'
+import 'nprogress/nprogress.css'
+import { start, done, configure } from 'nprogress'
+
+configure({
+  trickleSpeed: 20,
+  showSpinner: false
+})
+
+function delay(duration) {
+  return new Promise((resolve) => setTimeout(resolve, duration))
+}
+
+function getPageComponent(pageCompResolver) {
+  return async () => {
+    start()
+    if (process.env.NODE_ENV === 'development') {
+      await delay(2000)
+    }
+    const comp = await pageCompResolver()
+    done()
+    return comp
+  }
+}
 
 const routes = [
-  { name: 'home', path: '/', meta: { title: '首页' }, component: Home },
-  { name: 'about', path: '/about', meta: { title: '关于我' }, component: About },
-  { name: 'blog', path: '/blog', meta: { title: '文章' }, component: Blog },
+  {
+    name: 'home',
+    path: '/',
+    meta: { title: '首页' },
+    component: getPageComponent(() => import('@/views/Home/index.vue'))
+  },
+  {
+    name: 'about',
+    path: '/about',
+    meta: { title: '关于我' },
+    component: getPageComponent(() => import('@/views/About/index.vue'))
+  },
+  {
+    name: 'blog',
+    path: '/blog',
+    meta: { title: '文章' },
+    component: getPageComponent(() => import('@/views/Blog/index.vue'))
+  },
   {
     name: 'blogCategory',
     path: '/blog/cate/:categoryId',
     meta: { title: '文章' },
-    component: Blog
+    component: getPageComponent(() => import('@/views/Blog/index.vue'))
   },
-  { name: 'blogDetail', path: '/blog/:id', meta: { title: '文章详情' }, component: BlogDetail },
-  { name: 'message', path: '/message', meta: { title: '留言板' }, component: Message },
-  { name: 'project', path: '/project', meta: { title: '项目&详情' }, component: Project }
+  {
+    name: 'blogDetail',
+    path: '/blog/:id',
+    meta: { title: '文章详情' },
+    component: getPageComponent(() => import('@/views/Blog/index.vue'))
+  },
+  {
+    name: 'message',
+    path: '/message',
+    meta: { title: '留言板' },
+    component: getPageComponent(() => import('@/views/Message/index.vue'))
+  },
+  {
+    name: 'project',
+    path: '/project',
+    meta: { title: '项目&详情' },
+    component: getPageComponent(() => import('@/views/Project/index.vue'))
+  }
 ]
 
 export default routes
